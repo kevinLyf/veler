427be77452ccac898dfdb5c4684776524121bf73
Kevin Ferreira Feitosa <kevin.feitosa11@outlook.com>
2023-10-17 16:53:21 -0300

    feat: add dark theme on application

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:veler/features/screens/login_screen.dart';
+import 'package:veler/shared/themes/dark_theme.dart';
 
 class AppWidget extends StatelessWidget {
   const AppWidget({super.key});
@@ -9,10 +10,7 @@ class AppWidget extends StatelessWidget {
     return MaterialApp(
       title: 'Veler',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+      theme: darkTheme,
       home: const LoginScreen(),
     );
   }
