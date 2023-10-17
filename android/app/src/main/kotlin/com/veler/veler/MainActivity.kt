6fc7991bd20177bbee8eb209083acbc99612ab44
Kevin Ferreira Feitosa <kevin.feitosa11@outlook.com>
2023-10-17 16:47:33 -0300

    initial commit

---
+++ android/app/src/main/kotlin/com/veler/veler/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.veler.veler
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/app_widget.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:veler/features/screens/login_screen.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Veler',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const LoginScreen(),
+    );
+  }
+}
